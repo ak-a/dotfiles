85488ee6134d76f4d9b6927e5a1f353dc6104d31
Andrew King <ak-a@ak-a.com>
2020-05-12 15:47:42 +0100

    Fix debug of bash stuff

--- .bash/debug.bash
+++ .bash/debug.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-declare -a _debug_bashrc
 # shellcheck disable=SC2034
 #_debug_bashrc=( env_manip interactive misc_functions )
+_debug_bashrc=()
+#_debug_bashrc+=( keyring )
+export _debug_bashrc
